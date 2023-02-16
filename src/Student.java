01caae0e0125556014797211ba6c0f89a8c0c350
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-16 15:50:19 -0500

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private final String name;
+    private final int[] marks;
+
+    public Student(String name, int[] marks) {
+        this.name= name;
+        this.marks = marks; 
+    }
+}
---
+++ src/StudentRunner.java
@@ -0,0 +1,6 @@
+public class StudentRunner {
+    public static void main (String[] args) {
+        int[] marks = {99, 98,100};
+        Student student = new Student("Aarti", marks);
+    }
+}
